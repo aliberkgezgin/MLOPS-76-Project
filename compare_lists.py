5e13f8f7457a7fe3b847fcaea9840512646119ef
aliberkgezgin <aliberkgezgin@gmail.com>
2025-01-19 00:02:04 +0100

    removed unnecessary files

--- compare_lists.py
+++
@@ -1,7 +0,0 @@
-before = set(open("before_cleanup.txt").readlines())
-after = set(open("after_cleanup.txt").readlines())
-missing = before - after
-
-with open("missing_packages.txt", "w") as f:
-    f.writelines(sorted(missing))
-print("Missing packages saved to missing_packages.txt")
